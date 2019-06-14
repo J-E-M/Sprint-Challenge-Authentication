d52bd02db0cda69f58541c49bad69e980e5da64b
J-E-M <javonmccoy22@gmail.com>
2019-06-14 13:00:20 -0500

    it is done

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,23 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require("../knexfile.js");
 
-module.exports = knex(knexConfig.development);
+db = knex(knexConfig.development);
+
+module.exports = {
+  insert: user => {
+    return db("users").insert(user);
+  },
+
+  findByUsername: username => {
+    return db("users")
+      .where({ username })
+      .first();
+  },
+
+  findById: id => {
+    return db("users")
+      .where({ id })
+      .first();
+  }
+};
